a0c71d5c874d8699e39b75883b10dc6fdc755442
Edge <ilarumbgim1@educacion.navarra.es>
2022-05-26 19:46:48 +0200

    feat: temporary data and improvmeents to shop page

--- src/lib/shop-item.svelte
+++ src/lib/shop-item.svelte
@@ -1,20 +1,34 @@
 <script>
-import PrimaryButton from "./primary-button.svelte";
+	import PrimaryButton from './primary-button.svelte';
 
-
-    
+	// @ts-ignore
+	export let title;
+	export let price;
+	export let rare;
 </script>
-<div class=" overlay  transition-all duration-300 hover:scale-110 relative w-full rounded  shadow-xl ">
+
+<div
+	class=" overlay  transition-all duration-300 hover:scale-110 relative w-full rounded  shadow-xl "
+>
 	<img
 		src="https://polytoria.com/assets/thumbnails/catalog/15075.png?v=1"
 		class="object-cover rounded-t rounded-b-xl bg-zinc-900 w-full h-2/4"
 		alt=""
 	/>
 	<div class="p-2">
-		<div class="text-3xl font-bold ">Purple Top hat</div>
+		<div class="text-3xl font-bold ">{title}</div>
 		<div class="">By <span class="link">Owner</span></div>
-        
-        </div>
-        <a href="/buy/1" class="text-center  absolute bottom-5 w-full "><PrimaryButton>Buy</PrimaryButton></a>
-
+		<div class="flex gap-4">
+        {#if rare}
+			<div class="w-max rounded text-sm p-0.5 bg-yellow-500 text-white">LIMITED</div>
+		{/if}
+		<div class="font-bold text-green-600">
+			{price}
+			<li class="fa fa-dollar" />
+		</div>
+    </div>
+	</div>
+	<a href="/buy/1" class="text-center absolute bottom-2 w-full "
+		><PrimaryButton>Buy</PrimaryButton></a
+	>
 </div>
--- src/routes/shop/index.svelte
+++ src/routes/shop/index.svelte
@@ -1,26 +1,35 @@
 <script>
-import PrimaryButton from "$lib/primary-button.svelte";
+	import PrimaryButton from '$lib/primary-button.svelte';
 
-import ShopItem from "$lib/shop-item.svelte";
-
+	import ShopItem from '$lib/shop-item.svelte';
+	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
+	/**
+	 * @type {any}
+	 */
+	let data = writable([]);
+	let json = import("./data.json").then((JData) => {
+        console.log(JData.default)
+        data.set(JData.default)
+    })
 </script>
+
 <div class="p-3 flex items-center gap-1">
-    <PrimaryButton>Auctions</PrimaryButton>
-    <PrimaryButton>Categories</PrimaryButton>
-    <div class="flex items-center"><input type="text" class="secondary rounded-l h-fit p-[6px] outline-none w-64" placeholder="Item name or 'price:20, name:cool"><div class="primary p-[6px] rounded-r"><i class="fa-solid fa-magnifying-glass"></i></div></div>
+	<PrimaryButton>Auctions</PrimaryButton>
+	<PrimaryButton>Categories</PrimaryButton>
+	<div class="flex items-center">
+		<input
+			type="text"
+			class="secondary rounded-l h-fit p-[6px] outline-none w-64"
+			placeholder="Item name or 'price:20, name:cool"
+		/>
+		<div class="primary p-[6px] rounded-r"><i class="fa-solid fa-magnifying-glass" /></div>
+	</div>
 </div>
-    <div class="container  mt-5 grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-5 text-white mx-auto">
-       <ShopItem/>
-       <ShopItem/>
-       <ShopItem/>
-       <ShopItem/>
-       <ShopItem/>
-       <ShopItem/>
-       <ShopItem/>
-       <ShopItem/>
-       <ShopItem/>
-       <ShopItem/>
-       <ShopItem/>
-       <ShopItem/>
-       <ShopItem/>
-    </div>
+<div
+	class="container  mt-5 grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-5 text-white mx-auto"
+>
+	{#each $data as item}
+		<ShopItem title={item.AssetName} price={item.Price} rare={item.Limited} />
+	{/each}
+</div>
